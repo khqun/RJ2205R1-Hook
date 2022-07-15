96312336270f252cef27c88a86eb434bed3f928b
hzat <hzathzat@gmail.com>
2022-07-15 10:57:45 +0700

    change

--- hook-exercise/car-component/src/App.js
+++ hook-exercise/car-component/src/App.js
@@ -14,19 +14,23 @@ function App() {
   }
   return (
     <div>
+      <div>
       <select onChange={handlerCar}>
         <option value="Honda">Honda</option>
         <option value="Mercedes">Mercedes</option>
         <option value="Huyndai">Huyndai</option>
         <option value="Toyota">Toyota</option>
       </select>
+      </div>
+      <div>
       <select onChange={handlerColor}>
         <option value="red">red</option>
         <option value="blue">blue</option>
         <option value="black">black</option>
         <option value="white">white</option>
       </select>
-      <h1>Your car is: {selectedCar.color} {selectedCar.car}</h1>
+      </div>
+      <h1>Your car is: {selectedCar.color} - {selectedCar.car}</h1>
     </div>
   )
 }
--- hook-exercise/timer-component/src/component/timer.js
+++ hook-exercise/timer-component/src/component/timer.js
@@ -1,37 +1,17 @@
 import { useState, useEffect } from 'react';
 function Timer() {
-    const [time, setTime] = useState(4);
-    const [timeloop, setTimeloop] = useState();
-    const stopLoop = () => {
-        setTimeloop(null);
-    }
+    const [time, setTime] = useState(10);
     useEffect(() => {
-        setTimeloop(setInterval(() => {
+        const timeInterval = setInterval(() => {
             setTime(time - 1)
-        }, 1000)
-        )
-       
-        return function cleanup() {
-            console.log("clean");
-            clearInterval(timeloop)
-        }
-    }, [time])
-    if(time){
-        return (
-            <div>
-                <h1>Time: {time} seconds left</h1>
-            </div>
-        )
-    }
-    else {
-        stopLoop()
-        return ( 
-            <div>
-                <h1>End</h1>
-            </div>
-)
+        }, 1000);
+      if(!time){
+        clearInterval(timeInterval);
+      }
+    })
+    return <div>
+            <h1>Time left: {time}</h1>
+        </div>
 
-    }
-   
 }
 export default Timer;
